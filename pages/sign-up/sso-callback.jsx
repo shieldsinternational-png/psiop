99ce5a85c6a0e69993c032d7050a7e9dbfa32039
Atlas Sentinel <shieldsinternational@gmail.com>
2026-03-12 23:15:56 +0100

    fix pages folder structure

--- pages/sign-up/sso-callback.jsx
+++
@@ -1,7 +0,0 @@
-pages/
-  sign-in/
-    index.jsx  ← rename your current sign-in.jsx to index.jsx
-    sso-callback.jsx
-  sign-up/
-    index.jsx  ← rename your current sign-up.jsx to index.jsx
-    verify-email-address.jsx
