28ce7df4b60e99b50fc03629aed2b92667e0f2d1
Atlas Sentinel <shieldsinternational@gmail.com>
2026-03-13 00:38:23 +0100

    add reset-password route

---
+++ pages/sign-in/reset-password.jsx
@@ -0,0 +1,9 @@
+import { SignIn } from "@clerk/nextjs";
+
+export default function ResetPassword() {
+  return (
+    <div style={{ minHeight: "100vh", background: "#020a02", display: "flex", alignItems: "center", justifyContent: "center" }}>
+      <SignIn routing="path" path="/sign-in" fallbackRedirectUrl="https://www.psiop.io" />
+    </div>
+  );
+}
